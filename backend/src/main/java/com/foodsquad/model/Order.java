74fd244fedd372bd1a8e6d6abd6917cb74bab54c
ima-69 <imansha.idr@gmail.com>
2025-05-15 11:51:25 +0530

    created order entity

---
+++ backend/src/main/java/com/foodsquad/model/Order.java
@@ -0,0 +1,48 @@
+package com.foodsquad.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @ManyToOne
+    private User customer;
+
+    @JsonIgnore
+    @ManyToOne
+    private Restaurant restaurant;
+
+    private Long totalAmount;
+
+    private String orderStatus;
+
+    private Date createdAt;
+
+    @ManyToOne
+    private Address deliveryAddress;
+
+    @OneToMany
+    private List<OrderItem> items;
+
+    private Payment payment;
+
+    private int totalItem;
+
+    private int totalprice;
+
+
+}
