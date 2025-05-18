d548147296852642d7c102b4ae319f26d7ce720a
ima-69 <imansha.idr@gmail.com>
2025-05-18 20:20:51 +0530

    created authResponse file

---
+++ backend/src/main/java/com/foodsquad/response/AuthResponse.java
@@ -0,0 +1,12 @@
+package com.foodsquad.response;
+
+import com.foodsquad.model.enums.USER_ROLE;
+import lombok.Data;
+
+@Data
+public class AuthResponse {
+
+    private String jwt;
+    private String message;
+    private USER_ROLE role;
+}
