356df4aa104afe14a2ed4c075a533cf36169a5f1
ima-69 <imansha.idr@gmail.com>
2025-05-18 17:49:15 +0530

    created JwtConstant

---
+++ backend/src/main/java/com/foodsquad/config/JwtConstant.java
@@ -0,0 +1,6 @@
+package com.foodsquad.config;
+
+public class JwtConstant {
+    public static final String SECRET_KEY = "fvnvcmdhvbsncvdmvdvhgcnbcsgfjcdhkyjddsdvnvjsgnsbsjbfsjfcjlhcvmncxhjdbvxnxhvb";
+    public static final String JWT_HEADER = "Authorization";
+}
