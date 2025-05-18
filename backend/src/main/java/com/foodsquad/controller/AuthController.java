5ae9fd233ce7a856630d6480e53562f97cbd9776
ima-69 <imansha.idr@gmail.com>
2025-05-18 20:23:04 +0530

    completed sign up method

--- backend/src/main/java/com/foodsquad/controller/AuthController.java
+++ backend/src/main/java/com/foodsquad/controller/AuthController.java
@@ -1,17 +1,82 @@
 package com.foodsquad.controller;
 
+import com.foodsquad.config.JwtProvider;
+import com.foodsquad.model.Cart;
+import com.foodsquad.model.User;
+import com.foodsquad.repositoty.CartRepository;
 import com.foodsquad.repositoty.UserRepository;
+import com.foodsquad.response.AuthResponse;
+import com.foodsquad.service.CustomerUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+
 
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
 
+    @Autowired
     private UserRepository userRepository;
 
+    @Autowired
     private PasswordEncoder passwordEncoder;
 
+    @Autowired
+    private JwtProvider jwtProvider;
 
+    @Autowired
+    private CustomerUserDetailsService customerUserDetailsService;
+
+    @Autowired
+    private CartRepository cartRepository;
+
+    @PostMapping("/signup")
+    public ResponseEntity<AuthResponse> createUserHandler(@RequestBody User user) {
+
+        User isEmailExist = userRepository.findByEmail(user.getEmail());
+        if(isEmailExist != null) {
+            throw new RuntimeException("Email is already used with another account");
+        }
+
+        User createdUser = new User();
+        createdUser.setEmail(user.getEmail());
+        createdUser.setFirstName(user.getFirstName());
+        createdUser.setLastName(user.getLastName());
+        createdUser.setRole(user.getRole());
+        createdUser.setPassword(passwordEncoder.encode(user.getPassword()));
+
+        User savedUser = userRepository.save(createdUser);
+
+        Cart cart = new Cart();
+        cart.setCustomer(savedUser);
+        cartRepository.save(cart);
+
+        Authentication authentication = new UsernamePasswordAuthenticationToken(user.getEmail(), user.getPassword());
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        String jwt = jwtProvider.generateToken(authentication);
+
+        AuthResponse authResponse = new AuthResponse();
+
+        authResponse.setJwt(jwt);
+        authResponse.setMessage("User registered successfully");
+        authResponse.setRole(savedUser.getRole());
+
+        return new ResponseEntity<>(authResponse, HttpStatus.CREATED);
+    }
+
+    
 }
