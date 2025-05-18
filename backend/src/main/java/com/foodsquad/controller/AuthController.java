a2bfc4c2afe9341e041ae52bdb3d0ddac9b10091
ima-69 <imansha.idr@gmail.com>
2025-05-18 22:35:05 +0530

    created sign in functionality

--- backend/src/main/java/com/foodsquad/controller/AuthController.java
+++ backend/src/main/java/com/foodsquad/controller/AuthController.java
@@ -3,8 +3,10 @@ package com.foodsquad.controller;
 import com.foodsquad.config.JwtProvider;
 import com.foodsquad.model.Cart;
 import com.foodsquad.model.User;
+import com.foodsquad.model.enums.USER_ROLE;
 import com.foodsquad.repositoty.CartRepository;
 import com.foodsquad.repositoty.UserRepository;
+import com.foodsquad.request.LoginRequest;
 import com.foodsquad.response.AuthResponse;
 import com.foodsquad.service.CustomerUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,9 +14,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,6 +27,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collection;
 
 
 @RestController
@@ -78,5 +84,40 @@ public class AuthController {
         return new ResponseEntity<>(authResponse, HttpStatus.CREATED);
     }
 
-    
+    @PostMapping("/signin")
+    public ResponseEntity<AuthResponse> signInHandler(@RequestBody LoginRequest request) {
+
+        String username = request.getEmail();
+        String password = request.getPassword();
+
+        Authentication authentication = authenticate(username, password);
+
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        String role = authorities.iterator().next().getAuthority();
+
+        String jwt = jwtProvider.generateToken(authentication);
+
+        AuthResponse authResponse = new AuthResponse();
+        authResponse.setJwt(jwt);
+        authResponse.setMessage("User logged in successfully");
+
+        authResponse.setRole(USER_ROLE.valueOf(role));
+
+        return new ResponseEntity<>(authResponse, HttpStatus.OK);
+    }
+
+    private Authentication authenticate(String username, String password) {
+
+        UserDetails userDetails = customerUserDetailsService.loadUserByUsername(username);
+
+        if(userDetails == null) {
+            throw new BadCredentialsException("Invalid username.....");
+        }
+
+        if(!passwordEncoder.matches(password, userDetails.getPassword())) {
+            throw new BadCredentialsException("Invalid password.....");
+        }
+
+        return new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+    }
 }
