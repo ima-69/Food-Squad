51bd3419e5dc63685ecfe802ffaed05be551d0f1
ima-69 <imansha.idr@gmail.com>
2025-05-18 19:26:45 +0530

    Create AuthController.java

---
+++ backend/src/main/java/com/foodsquad/controller/AuthController.java
@@ -0,0 +1,17 @@
+package com.foodsquad.controller;
+
+import com.foodsquad.repositoty.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    private UserRepository userRepository;
+
+    private PasswordEncoder passwordEncoder;
+
+
+}
